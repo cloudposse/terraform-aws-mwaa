5d1c07aa28d2672a3b4288ce03558b48c1e09f94
Hugo Samayoa <htplbc@gmail.com>
2021-12-22 15:28:29 -0800

    attribute is causing weird issues with names

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -23,9 +23,9 @@ func TestExamplesComplete(t *testing.T) {
 		VarFiles: []string{"fixtures.us-east-2.tfvars"},
 		// We always include a random attribute so that parallel tests
 		// and AWS resources do not interfere with each other
-		Vars: map[string]interface{}{
-			"attributes": attributes,
-		},
+		//Vars: map[string]interface{}{
+		//	"attributes": attributes,
+		//},
 	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
