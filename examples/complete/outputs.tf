4c0e9e324e63f76a8462da78d6aea2b06eb20ce6
Hugo Samayoa <htplbc@gmail.com>
2021-12-22 13:01:55 -0800

    update output name

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -5,5 +5,5 @@ output "mwaa_environment_arn" {
 
 output "mwaa_environment_logging_configuration" {
   description = "The Created At date of the MWAA Environment"
-  value       = module.mwaa.logging_configuration
+  value       = module.mwaa.mwaa_environment_logging_configuration
 }
