fc79d18d84827727462381ac002c9a52d8239611
Hugo Samayoa <htplbc@gmail.com>
2022-01-24 14:30:06 -0800

    add associated_security_group_ids

--- variables.tf
+++ variables.tf
@@ -38,12 +38,21 @@ variable "source_bucket_arn" {
   default     = null
 }
 
-variable "allowed_security_group_ids" {
+variable "associated_security_group_ids" {
   type        = list(string)
   default     = []
   description = <<-EOT
     A list of IDs of Security Groups to associate the created resource with, in addition to the created security group.
     These security groups will not be modified and, if `create_security_group` is `false`, must have rules providing the desired access.
+    EOT
+}
+
+variable "allowed_security_group_ids" {
+  type        = list(string)
+  default     = []
+  description = <<-EOT
+    A list of IDs of Security Groups to allow access to the security group created by this module.
+    The length of this list must be known at "plan" time.
     EOT
 }
 
